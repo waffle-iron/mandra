6ab674ac6380becb209acdb8bbf12b820bd45833
Randson Oliveira <orandson@gmail.com>
2016-06-19 16:58:21 -0300

    Add paths for js and docs

--- src/tasks/paths.js
+++ src/tasks/paths.js
@@ -1,10 +1,35 @@
 module.exports = {
+  public: 'public/',
   src: {
     css: {
       src: 'src/css',
       main: 'src/css/mandra.css',
       glob: 'src/css/**/*.css',
       dest: 'dist/css/'
+    },
+    js: {
+      src: 'src/js/',
+      main: 'src/js/entry.js',
+      glob: 'src/js/**/*.js',
+      dest: 'dist/js/'
+    }
+  },
+  docs: {
+    css: {
+      src: 'docs/styles/',
+      main: 'docs/styles/main.css',
+      glob: 'docs/styles/**/*.css',
+      dest: 'public/css/'
+    },
+    js: {
+      dest: 'public/js'
+    },
+    layout: {
+      src: 'docs/layout/',
+      glob: 'docs/layout/**/*.jade'
+    },
+    pages: {
+      glob: 'docs/**/*.md'
     }
   }
 }
